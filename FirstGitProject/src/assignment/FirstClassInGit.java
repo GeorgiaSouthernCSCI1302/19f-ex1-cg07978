0bfa458fcac914d1ea1c88b60ccdd8ef5b38b506
cg07978 <cg07978@CEIT2553214X004.ad.georgiasouthern.edu>
2019-08-21 16:34:46 -0400

    Added a statement that displays my name in the console.

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -6,7 +6,7 @@ public class FirstClassInGit {
 		System.out.println("Hello World!");
 		
 		//add a statement to display your name in the console
-		
+		System.out.print("My name is Charles.");
 		//Then commit your changes
 
 	}
