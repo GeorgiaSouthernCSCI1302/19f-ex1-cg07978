0ffc112af11632bc33fadd0d2dc6c3f852c3b992
cg07978 <cg07978@CEIT2553214X004.ad.georgiasouthern.edu>
2019-08-21 16:44:36 -0400

    Stored user's last name and printed it.

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,13 +1,19 @@
 package assignment;
-
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
 		//add a statement to display your name in the console
-		System.out.print("My name is Charles.");
+		System.out.println("My name is Charles.");
 		//Then commit your changes
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is your last name?");
+		String name = input.nextLine();
+		input.close();
+		System.out.print("Your last name is " + name + ".");
+		
 
 	}
 
